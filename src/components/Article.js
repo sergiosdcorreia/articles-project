e6ef4d83432f8059b112218d10c3afbeb95b7b20
Sergio Correia <sergiosdcorreia@gmail.com>
2019-07-28 20:58:17 +0100

    Added documentation to the README file. Some layout adjustments.

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,28 +3,19 @@ import { Link } from "react-router-dom";
 import ArticleData from '../data/articles.json';
 import './Article.css';
 
-class Article extends React.Component {
-  constructor(props){
-    super(props);
+function Article(props) {
 
-    console.log(this.props);
-  }
+  let article = ArticleData[props.id];
 
-    render() {
-      let article = ArticleData[this.props.id];
-      console.log(article);
-
-      return (
-        <div className="article">
-          <h2 className="article__title">{article.title}</h2>
-          <h3 className="article__description">{article.description}</h3>
-          <img className="article__image" src={article.image} alt={article.title} />
-          <p className="article__body">{article.body}</p>
-          <Link to="/" className="article__link">Back</Link>
-        </div>
-      );
-    }
-
-  }
+  return (
+    <div className="article">
+      <h2 className="article__title">{article.title}</h2>
+      <h3 className="article__description">{article.description}</h3>
+      <img className="article__image" src={article.image} alt={article.title} />
+      <p className="article__body">{article.body}</p>
+      <Link to="/" className="article__link">Back</Link>
+    </div>
+  );
+}
   
   export default Article;
--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -4,27 +4,26 @@ import './ArticleList.css';
 
 function ArticlesList(props) {
 
-    const { ArticleData} = props;
+  const { ArticleData } = props;
 
-    const articlesListMap = ArticleData.map(({ id, title, description }) => {
-        return (
-            <div key={id} className="article-list">
-            
-                <div className="inner-container">
-                    <div className="article-list__text">
-                        <h2 className="article-list__title">{title}</h2>
-                        <h3 className="article-list__description">{description}</h3>
-                    </div>
-                    <Link to={`/articles/${id}`} className="article-list__link">Read more</Link>
-                </div>
-            </div>
-        )
-    });
+  const articlesListMap = ArticleData.map(({ id, title, description }) => {
+    return (
+      <div key={id} className="article-list">
+        <div className="inner-container">
+          <div className="article-list__text">
+            <h2 className="article-list__title">{title}</h2>
+            <h3 className="article-list__description">{description}</h3>
+          </div>
+          <Link to={`/articles/${id}`} className="article-list__link">Read more</Link>
+        </div>
+      </div>
+    )
+  });
 
   return (
-      <div className="container">
-        {articlesListMap}
-      </div>
+    <div className="container">
+      {articlesListMap}
+    </div>
   )
 };
 
